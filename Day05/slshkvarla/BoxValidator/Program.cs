46afda1164820900e85a5e6c36e198a70d0d7266
Самуил Любославов Шкварла <SLShkvarla20@codingburgas.bg>
2023-11-06 16:56:58 +0200

    Add slshkvarla folder

---
+++ Day05/slshkvarla/BoxValidator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BoxValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input: 2 -3 4
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(lenght, height, width);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
