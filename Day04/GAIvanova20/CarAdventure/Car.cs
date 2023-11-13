0cdd55ca49ea6b80c71dcc2fda64608bde240d8d
GAIvanova20 <GAIvanova20@codingburgas.bg>
2023-11-13 17:06:07 +0200

    Add CarAdventure class

---
+++ Day04/GAIvanova20/CarAdventure/Car.cs
@@ -0,0 +1,37 @@
+﻿namespace CarAdventure
+{
+    public class Car 
+    {
+        public string Model { get; set; }
+        public double Fuel { get; set; }
+        public double FuelConsumption { get; set; }
+        public double Distance { get; set; }
+
+        public Car(string model, double fuel, double fuelConsumption) 
+        {
+            this.Model = model;
+            this.Fuel = fuel;
+            this.FuelConsumption = fuelConsumption;
+            this.Distance = 0;
+        }
+
+        public void drive(double distance)
+        {
+            double fuelNeeded = FuelConsumption * distance;
+            if (Fuel > fuelNeeded)
+            {
+                Fuel -= fuelNeeded;
+                Distance += distance;
+            }
+            else 
+            {
+                Console.WriteLine("Insufficient fuel for the driver");
+            }
+        }
+
+        public void printCarInfo()
+        {
+            Console.WriteLine($"{Model} {Fuel} {Distance}");
+        }
+    }
+}
---
+++ Day04/GAIvanova20/CarAdventure/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CarAdventure
+{
+    public partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car[] cars = new Car[100];
+
+            string[] carInfo, commandLine;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                carInfo = Console.ReadLine().Split(" ").ToArray();
+
+                cars[i] = new Car(carInfo[0], double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+            }
+
+            commandLine = Console.ReadLine().Split(" ").ToArray();
+
+            while (commandLine[0] != "End")
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (commandLine[0] == "Drive" && cars[i].Model == commandLine[1])
+                    {
+                        cars[i].drive(double.Parse(commandLine[2]));
+                    }
+                }
+                commandLine = Console.ReadLine().Split(" ").ToArray();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                cars[i].printCarInfo();
+            }
+        }
+    }
+}
