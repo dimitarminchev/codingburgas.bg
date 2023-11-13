27d65cda3c409f164e50617c96115b3d3d8fa5f2
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-11-13 15:16:36 +0200

    DAy 4

---
+++ Day04/Demo/Dice/Dice.cs
@@ -0,0 +1,34 @@
+﻿namespace Dice
+{
+    /// <summary>
+    /// Зарче
+    /// </summary>
+    public class Dice
+    {
+        /// <summary>
+        /// Страни
+        /// </summary>
+        public int sides;
+
+        private Random rnd = new Random();
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="sides">Брой страни</param>
+        public Dice(int sides = 6)
+        {
+            this.sides = sides;
+        }
+
+        /// <summary>
+        /// Хвърляне на зара
+        /// </summary>
+        /// <returns>Случайно число от 1 до броя страни</returns>
+        public int Roll()
+        {
+            int rollResult = rnd.Next(1, this.sides + 1);
+            return rollResult;
+        }
+    }
+}
---
+++ Day04/Demo/Dice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Dice
+{
+    /// <summary>
+    /// Главна програма
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(dice.Roll());
+            }
+        }
+    }
+}
