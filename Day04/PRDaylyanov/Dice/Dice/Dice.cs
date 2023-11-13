6854d067f3a2fef7fe88986b4e695cc870a2f091
Plamen <82028748+ShadowCode07@users.noreply.github.com>
2023-11-13 15:32:12 +0200

    PRDaylyanov Add Dice

---
+++ Day04/PRDaylyanov/Dice/Dice/Dice.cs
@@ -0,0 +1,32 @@
+﻿namespace Dice
+{
+    public class Dice
+    {
+        private int[] rollFrequency;
+
+        private Random rnd = new Random();
+
+        public int sides;
+
+        public Dice(int sides = 6)
+        {
+            this.sides = sides;
+            this.rollFrequency = new int[sides + 1];
+        }
+
+        public int Roll()
+        {
+            int rollResult = rnd.Next(1, this.sides + 1);
+            this.rollFrequency[rollResult]++;
+            return rollResult;
+        }
+
+        public void PrintFrequency()
+        {
+            for(int i = 1; i <= this.sides; i++)
+            {
+                Console.WriteLine($"{i} => {this.rollFrequency[i]}");
+            }
+        }
+    }
+}
---
+++ Day04/PRDaylyanov/Dice/Dice/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Dice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(dice.Roll());
+            }
+            dice.PrintFrequency();
+        }
+    }
+}
