aca783aa33b62a34f00f5ca686e6b129ebdd1582
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-11-13 16:11:11 +0200

    Day 4

---
+++ Day04/Demo/RawData/Load.cs
@@ -0,0 +1,18 @@
+﻿namespace RawData
+{
+    /// <summary>
+    /// Товар
+    /// </summary>
+    public class Load
+    {
+        /// <summary>
+        /// Тегло на товара
+        /// </summary>
+        public int Weight { get; set; }
+
+        /// <summary>
+        /// Тип на товара
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
