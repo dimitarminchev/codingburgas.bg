7ee032f9f8320b39a3000611c021e40e3adb24dc
Калоян Димов <KHDimov20@codingburgas.bg>
2023-11-13 17:02:52 +0200

    Add CarRide system

---
+++ Day04/KaloyanDimov/CarRide/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRide
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double fuel { get; set; }
+        public double fuelRate { get; set;}
+        public int distance { get; set; }
+        public Car(string model, double fuel, double fuelRate, int distance) 
+        {
+            this.Model = model;
+            this.fuel = fuel;
+            this.fuelRate = fuelRate;
+            this.distance = distance;
+        }
+        public void Drive(int distance) 
+        {
+            if (distance * this.fuelRate < this.fuel)
+            {
+                this.fuel -= distance *this.fuelRate;
+                this.distance += distance;
+            }
+            else 
+            {
+                Console.WriteLine("Insufficient fuel for this drive");
+            }
+        }
+        public void EndPrint(string Model) 
+        {
+            Console.WriteLine($"{Model} {this.fuel} {this.distance}");
+        }
+    }
+}
---
+++ Day04/KaloyanDimov/CarRide/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CarRide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < n; i++) 
+            {
+                string[] line = Console.ReadLine().Split(' ').ToArray();
+                Car car = new Car(line[0], double.Parse(line[1]), double.Parse(line[2]), 0);
+                cars.Add(car);
+            }
+            string[] line1 = Console.ReadLine().Split(' ').ToArray();
+            while (line1[0] != "End") 
+            {
+                foreach (Car car in cars) 
+                {
+                    if (car.Model == line1[1]) 
+                    {
+                        car.Drive(int.Parse(line1[2]));
+                    }
+                }
+                line1 = Console.ReadLine().Split(' ').ToArray();
+            };
+            foreach (Car car in cars) 
+            {
+                car.EndPrint(car.Model);
+            }
+        }
+    }
+}
