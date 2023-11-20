b7673fcbef1304b7b8df2331029c05f2343bdd7b
Plamen <PRDaylyanov20@codingburgas.bg>
2023-11-20 15:42:52 +0200

    Add day7 PRDaylyanov20

---
+++ Day04/PRDaylyanov/Day4/CarTravel/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace CarTravel
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double Fuel { get; set; }
+
+        public double FuelPerKm { get; set; }
+
+        public double DistanceTravelled { get; set; }
+
+        public Car(string model, double fuel, double fuelPerKm)
+        {
+            Model = model;
+            Fuel = fuel;
+            FuelPerKm = fuelPerKm;
+        }
+
+        public void Travel(double distance)
+        {
+            double fuelConsumed = distance * this.FuelPerKm;
+
+            if(fuelConsumed > this.Fuel)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                this.DistanceTravelled += distance;
+                this.Fuel -= fuelConsumed;
+                Console.WriteLine($"{this.Model}{this.Fuel}{this.DistanceTravelled}");
+            }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> Cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] carInput = Console.ReadLine().Split().ToArray();
+
+                Cars.Add(
+                    new Car{ 
+                        Model = carInput[0],
+                        Fuel = double.Parse(carInput[1]),
+                        FuelPerKm = double.Parse(carInput[2])
+                    }
+                ));
+            }
+        }
+    }
+}
