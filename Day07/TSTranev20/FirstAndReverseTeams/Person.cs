f98c538e896b28afe7609063c89bbf85726a6dec
ERKeserdzhieva20 <ERKeserdzhieva20@codingburgas.bg>
2023-11-20 15:41:50 +0200

    firAdd files

---
+++ Day07/TSTranev20/FirstAndReverseTeams/Person.cs
@@ -0,0 +1,12 @@
+namespace FirstAndReverseTeams;
+
+public class Person
+{
+    public string FirstName { get; set; }
+ 
+    public string LastName { get; set; }
+ 
+    public int Age { get; set; }
+ 
+    public double Salary { get; set; }
+}
