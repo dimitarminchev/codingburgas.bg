fcb2a8f22db36d1929911d57e9fe31583be964a3
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-11-20 16:33:49 +0200

    Day 7

---
+++ Day07/Demo/FootballTeam/Team.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace FootballTeam
+{
+    /// <summary>
+    /// Екип
+    /// </summary>
+    public class Team
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value; 
+            }
+        }
+
+        public List<Player> Players { get; set; }
+
+        public decimal Rating { get; set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Име на екип</param>
+        public Team(string name)
+        {
+            this.name = name;
+            this.Players = new List<Player>();
+        }
+    }
+}
