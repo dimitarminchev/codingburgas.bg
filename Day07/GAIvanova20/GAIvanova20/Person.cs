8731ded9be93eb0b8bb289740244bff094505cae
GAIvanova20 <GAIvanova20@codingburgas.bg>
2023-11-20 15:32:56 +0200

    Add FirstAndReserveTeam class

---
+++ Day07/GAIvanova20/GAIvanova20/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace GAIvanova20
+{
+    public class Person
+    {
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+
+        public int age { get; set; }
+
+        public double salary { get; set; }
+    }
+}
---
+++ Day07/GAIvanova20/GAIvanova20/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GAIvanova20
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Team team = new Team("codingburgas");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split().ToArray();
+
+                team.AddPlayer(new Person
+                {
+                    firstname = line[0],
+                    lastname = line[1],
+                    age = int.Parse(line[2]),
+                    salary = double.Parse(line[3])
+                });
+            }
+
+            Console.WriteLine($"First team have {team.FirstTeam.Count} players");
+            Console.WriteLine($"Reserved team have {team.ReserveTeam.Count} players");
+        }
+    }
+}
