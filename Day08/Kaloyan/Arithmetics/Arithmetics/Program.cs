f95abcf7f51ba1b2f60f945f00ae27a993dc3e1a
Калоян Димов <KHDimov20@codingburgas.bg>
2023-11-27 15:40:54 +0200

    Add Demo exercieses

---
+++ Day08/Kaloyan/Arithmetics/Arithmetics/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ').ToArray();
+            int a = int.Parse(line[0]), b = int.Parse(line[1]);
+            Console.WriteLine($"{a} + {b} = {Arithmetics.Add(a, b)}");
+            Console.WriteLine($"{a} * {b} = {Arithmetics.Multiply(a, b)}");
+            Console.WriteLine($"{a} - {b} = {Arithmetics.Subtract(a, b)}");
+            Console.WriteLine($"{a} / {b} = {Arithmetics.Divide(a, b)}");
+        }
+    }
+}
