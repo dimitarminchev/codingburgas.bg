6bffb0817b8d3ddce0334d915251d6e7d19213b5
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-11-27 15:37:44 +0200

    Day 08

---
+++ Day08/Demo/SquareRootPrecalculator/SquareRootPrecalculator.cs
@@ -0,0 +1,36 @@
+﻿namespace SquareRootPrecalculator
+{
+    /// <summary>
+    /// Клас за пресмятане на корени
+    /// </summary>
+    public static class SquareRootPrecalculator
+    {
+        // Максимална стойност, за която ще пресмятаме корените
+        public const int MaxValue = 100000;
+
+        // Масив, в който ще пазим пресметнатите стойности
+        private static double[] sqrtValues;
+
+        /// <summary>
+        /// Статичен конструктор, в който ще пресметнем корените
+        /// </summary>
+        static SquareRootPrecalculator()
+        {
+            sqrtValues = new double[MaxValue + 1];
+            for (int i = 1; i <= MaxValue; i++)
+            {
+                sqrtValues[i] = Math.Sqrt(i);
+            }
+        }
+
+        /// <summary>
+        /// Връща корен квадратен корен от дадено число
+        /// </summary>
+        /// <param name="value">Числото</param>
+        /// <returns>Корен квадратен на числото</returns>
+        public static double GetSqrt(int value)
+        {
+            return sqrtValues[value];
+        }
+    }
+}
