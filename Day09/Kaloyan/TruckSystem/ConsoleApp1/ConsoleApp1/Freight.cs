622cbb4062687f91e1bec23b27f4c3526353e16c
Калоян Димов <KHDimov20@codingburgas.bg>
2023-12-04 17:05:04 +0200

    Add Truck system

---
+++ Day09/Kaloyan/TruckSystem/ConsoleApp1/ConsoleApp1/Freight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckSystem
+{
+    public class Freight
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+    }
+}
---
+++ Day09/Kaloyan/TruckSystem/ConsoleApp1/ConsoleApp1/Truck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckSystem
+{
+    public class Truck
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private double loadCapacity;
+
+        public double LoadCapacity
+        {
+            get { return loadCapacity; }
+            set { loadCapacity = value; }
+        }
+
+        public List<Freight> freightQuantity = new List<Freight>();
+
+
+        public void Load(Freight freight)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (freight.Weight <= this.loadCapacity)
+            {
+                this.loadCapacity -= freight.Weight;
+                this.freightQuantity.Add(freight);
+                Console.WriteLine($"{this.name} loaded {freight.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.name} can not load {freight.Name}");
+            }
+        }
+
+        public void ShowLoadedFreights()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (freightQuantity.Count() > 0)
+            {
+                Console.Write($"{this.name} - {freightQuantity[0].Name}");
+                for (int i = 1; i < freightQuantity.Count(); i++)
+                {
+                    Console.Write($", {freightQuantity[i].Name}");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"{this.name} - Nothing loaded");
+            }
+        }
+    }
+}
