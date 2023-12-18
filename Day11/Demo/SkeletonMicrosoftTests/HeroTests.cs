90beb68db79245014472ff810dcf5a208e224582
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-12-18 16:23:19 +0200

    Day 11

--- Day11/Demo/SkeletonMicrosoftTests/HeroTests.cs
+++ Day11/Demo/SkeletonMicrosoftTests/HeroTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace SkeletonMicrosoftTests
 {
@@ -8,7 +9,7 @@ namespace SkeletonMicrosoftTests
         private const string HeroName = "Pesho";
 
         [TestMethod]
-        public void HeroGainsExperienceAfterAttackIfTargetDies()
+        public void TestHeroGainsExperienceAfterAttackIfTargetDies()
         {
             // Arrange
             ITarget fakeTarget = new FakeTarget(); // Health = 0, Experience = 20, Dead = true
@@ -21,5 +22,27 @@ namespace SkeletonMicrosoftTests
             // Assert
             Assert.AreEqual(20, hero.Experience, "Hero Must Gain Experience After Attack.");
         }
+
+        [TestMethod]
+        public void TestHeroGainsExperienceAfterAttackIfTargetDiesUsingMock()
+        {
+            // Arrange
+            Mock<ITarget> fakeTarget = new Mock<ITarget>();
+            fakeTarget.Setup(t => t.Health).Returns(0);
+            fakeTarget.Setup(t => t.GiveExperience()).Returns(20);
+            fakeTarget.Setup(t => t.IsDead()).Returns(true);
+
+            Mock<IWeapon> fakeWeapon = new Mock<IWeapon>();
+            fakeWeapon.Setup(w => w.AttackPoints).Returns(10);
+            fakeWeapon.Setup(w => w.DurabilityPoints).Returns(10);
+
+            // Act
+            Hero hero = new Hero(HeroName, fakeWeapon.Object);
+            hero.Attack(fakeTarget.Object);
+
+            // Assert
+            Assert.AreEqual(20, hero.Experience, "Hero Must Gain Experience After Attack.");
+        }
+
     }
 }
--- Day11/readme.md
+++ Day11/readme.md
@@ -1,3 +1,4 @@
 # Day 11
 - Unit Testing: Best Practices
 - Unit Testing: Dependency Injection
+- Unit Testing: Mocking objects
