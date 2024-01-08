b25372135a734d444d0f127e7f59ae0a90dced28
Самуил Любославов Шкварла <SLShkvarla20@codingburgas.bg>
2024-01-08 16:39:54 +0200

    Add Numbers exercises

---
+++ Day12/slshkvarla20/Numbers/NumberK/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace NumberK
+{
+    class EvenNumber
+    {
+        private double num;
+
+        public double Num
+        {
+            get { return num; }
+            set { 
+                num = value; }
+        }
+
+        public EvenNumber(double num)
+        {
+            this.Num = num;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<EvenNumber> nums = new List<EvenNumber>();
+
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            foreach (int i in input)
+            {
+                nums.Add(new EvenNumber(i));
+            }
+
+            int k = int.Parse(Console.ReadLine());
+            foreach (var i in nums)
+            {
+                if (i.Num % k == 0)
+                {
+                    Console.Write(i.Num + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Day12/slshkvarla20/Numbers/RacionalNumber/RacionalNumber.cs
@@ -0,0 +1,27 @@
+﻿namespace RacionalNumber
+{
+    class RacionalNumber
+    {
+        private double numerator;
+
+        public double Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+
+        private double denuminator;
+
+        public double Denuminator
+        {
+            get { return denuminator; }
+            set { denuminator = value; }
+        }
+
+        public RacionalNumber(double numerator, double denumerator)
+        {
+            this.Numerator = numerator;
+            this.Denuminator = denumerator;
+        }
+    }
+}
