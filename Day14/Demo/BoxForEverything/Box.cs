33cc2817fcf2c07c84f32cb5284f18f7fc391d13
Dimitar Minchev <dimitar.minchev@gmail.com>
2024-01-29 15:23:56 +0200

    Day 14

---
+++ Day14/Demo/BoxForEverything/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace BoxForEverything
+{
+    public class Box<T>
+    {
+        private T value { get; set; }
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{value.GetType().FullName}: {value}";
+        }
+    }
+}
---
+++ Day14/Demo/BoxForEverything/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BoxForEverything
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // 2. Кутия за всичко
+            var integerBox = new Box<int>(123123);
+            Console.WriteLine(integerBox.ToString());
+
+            var stringBox = new Box<string>("life in a box");
+            Console.WriteLine(stringBox.ToString());
+            */
+
+            /*
+            // 3. Универсална кутия за низове
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var box3 = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box3.ToString());
+            }
+            */
+
+            // 4. Универсална кутия за цели числа
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                var box4 = new Box<int>(number);
+                Console.WriteLine(box4.ToString());
+            }
+        }
+    }
+}
