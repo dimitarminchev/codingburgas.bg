12bd580be7387ecea2b2d7ac5d9f01851f19dda3
Dimitar Minchev <dimitar.minchev@gmail.com>
2024-02-26 15:20:27 +0200

    Day 17

---
+++ Day17/Demo/Figures/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Figures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Кръг
+            Console.Write("R=");
+            int radius = int.Parse(Console.ReadLine());
+
+            IDrawable circle = new Circle(radius);
+
+            circle.Draw();
+
+            // Правоъгълник
+            Console.Write("W=");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("H=");
+            int height = int.Parse(Console.ReadLine());
+
+            IDrawable rect = new Rectangle(width, height);
+
+            rect.Draw();
+        }
+    }
+}
---
+++ Day17/Demo/Figures/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿namespace Figures
+{
+    /// <summary>
+    /// Правоъгълник
+    /// </summary>
+    public class Rectangle : IDrawable
+    {
+        private int Width, Height;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="w">Широчина</param>
+        /// <param name="h">Височина</param>
+        public Rectangle(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+
+        /// <summary>
+        /// Рисувай
+        /// </summary>
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+            for (int i = 1; i < this.Height - 1; ++i)
+                DrawLine(this.Width, '*', ' ');
+            DrawLine(this.Width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+
+    }
+}
